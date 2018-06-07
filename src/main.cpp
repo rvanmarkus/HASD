99fad153ec29cd4a11fb165e5d2254d9e2eaba68
Robbert van Markus <rvanmarkus@stackstate.com>
2018-06-07 19:17:27 +0200

    change to only subscribe to topics not publish

--- src/main.cpp
+++ src/main.cpp
@@ -3,85 +3,109 @@
 #include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
 #include "OLEDDisplayUi.h"
 #include <Hash.h>
-
 #include "images.h"
-
-// Initialize the OLED display using SPI
-// D5 -> CLK
-// D7 -> MOSI (DOUT)
-// D0 -> RES
-// D2 -> DC
-// D8 -> CS
-// SSD1306Spi        display(D0, D2, D8);
-// or
-// SH1106Spi         display(D0, D2);
-
-// Initialize the OLED display using brzo_i2c
-// D3 -> SDA
-// D5 -> SCL
-// SSD1306Brzo display(0x3c, D3, D5);
-// or
-// SH1106Brzo  display(0x3c, D3, D5);
-
-// Initialize the OLED display using Wire library
+#include <Encoder.h>
 #include <esphomelib/application.h>
-// SH1106 display(0x3c, D3, D5);
-SSD1306 display(0x3c, D2, D1);
+
+SSD1306 display(0x3c, D6, D5);
 OLEDDisplayUi ui(&display);
 
-esphomelib::Application::MakeGPIOBinarySensor switchSensor;
+// Encoder myEnc(D1, D2);
 char *currentNotification = "No notifications";
+char *currentScene = "No scene activated";
+char *currentMediaInput = "No input media";
+
 using namespace esphomelib;
+Application::MakeGPIOBinarySensor switchSensor;
+mqtt::MQTTClientComponent *mqttClient;
+
+long oldPosition = -999;
+bool isButtonPressed = false;
+long lastUpdateMillis = 0;
+
 bool showNotification = false;
 int shownNotificationCounter = 0;
 
-void msOverlay(OLEDDisplay *display, OLEDDisplayUiState* state) {
-//   display->setTextAlignment(TEXT_ALIGN_RIGHT);
-//   display->setFont(ArialMT_Plain_10);
-//   display->drawString(128, 0, String(millis()));
-    if (showNotification) {
-        display->drawXbm(112,0, 16, 16, alarm);
+bool partyButtonActivated = false;
+
+
+void msOverlay(OLEDDisplay *display, OLEDDisplayUiState *state)
+{
+    //   display->setTextAlignment(TEXT_ALIGN_RIGHT);
+    //   display->setFont(ArialMT_Plain_10);
+    //   display->drawString(128, 0, String(millis()));
+    display->drawHorizontalLine(0, 20, 128);
+    if (showNotification)
+    {
+        display->drawXbm(112, 0, 16, 16, alarm);
+    }
+
+    if (partyButtonActivated)
+    {
+        display->drawXbm(112, 0, 16, 16, party);
     }
 }
 
-void drawLightsFrame(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
-  // draw an xbm image.
-  // Please note that everything that should be transitioned
-  // needs to be drawn relative to x and y
+void drawScenesFrame(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y)
+{
+    // Text alignment demo
 
-  display->drawXbm(x + 34, y + 14, WiFi_Logo_width, WiFi_Logo_height, WiFi_Logo_bits);
+    // The coordinates define the left starting point of the text
+    display->setTextAlignment(TEXT_ALIGN_LEFT);
+    display->setFont(ArialMT_Plain_10);
+    display->drawString(0 + x, 0 + y, "Huidige scene: ");
+    // display->drawString(0 + x, 15 + y, "Notification:");
+    display->setFont(ArialMT_Plain_16);
+    display->drawStringMaxWidth(0 + x, 24 + y, 128, currentScene);
+}
+void drawMediaInputFrame(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y)
+{
+    // Text alignment demo
+
+    // The coordinates define the left starting point of the text
+    display->setTextAlignment(TEXT_ALIGN_LEFT);
+    display->setFont(ArialMT_Plain_10);
+    display->drawString(0 + x, 0 + y, "Media input: ");
+    // display->drawString(0 + x, 15 + y, "Notification:");
+    display->setFont(ArialMT_Plain_16);
+    display->drawStringMaxWidth(0 + x, 24 + y, 128, currentMediaInput);
 }
 
-void drawNotificationFrame(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
-  // Text alignment demo
+void drawNotificationFrame(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y)
+{
+    // Text alignment demo
 
-  // The coordinates define the left starting point of the text
-  display->setTextAlignment(TEXT_ALIGN_LEFT);
-  display->setFont(ArialMT_Plain_10);
-  display->drawString(0 + x, 10 + y, "Notification:");
-  display->setFont(ArialMT_Plain_16);
-  display->drawStringMaxWidth(0 + x, 24 + y, 128, currentNotification);
-
-  // The coordinates define the center of the text
-//   display->setTextAlignment(TEXT_ALIGN_CENTER);
-//   display->drawString(64 + x, 22 + y, "Center aligned (64,22)");
-
-  // The coordinates define the right end of the text
-//   display->setTextAlignment(TEXT_ALIGN_RIGHT);
-//   display->drawString(128 + x, 33 + y, "Right aligned (128,33)");
+    // The coordinates define the left starting point of the text
+    display->setTextAlignment(TEXT_ALIGN_LEFT);
+    display->setFont(ArialMT_Plain_10);
+    display->drawString(0 + x, 0 + y, "Last notification: ");
+    // display->drawString(0 + x, 15 + y, "Notification:");
+    display->setFont(ArialMT_Plain_16);
+    display->drawStringMaxWidth(0 + x, 24 + y, 128, currentNotification);
+}
+void onSceneButtonPressed()
+{
+    mqttClient->publish("hasd/scene/cmd", "TOGGLE", 0, false);
 }
 
+void onMediaInputButtonPressed()
+{
+    mqttClient->publish("hasd/media/cmd", "TOGGLE", 0, false);
+}
 
-// This array keeps function pointers to all frames
-// frames are the single views that slide in
-FrameCallback frames[] = { drawNotificationFrame, drawLightsFrame };
-
+FrameCallback frames[] = {drawNotificationFrame, drawScenesFrame, drawMediaInputFrame};
+void (*frameButtonPressCallbacks[])() = {onSceneButtonPressed, onMediaInputButtonPressed, onSceneButtonPressed};
 // how many frames are there?
-int frameCount = 2;
+int frameCount = 3;
 
 // Overlays are statically drawn on top of a frame eg. a clock
-OverlayCallback overlays[] = { msOverlay };
+OverlayCallback overlays[] = {msOverlay};
 int overlaysCount = 1;
+void handleKey()
+{
+    ui.nextFrame();
+    
+}
 
 void onNotification(const std::string &message)
 {
@@ -91,7 +115,28 @@ void onNotification(const std::string &message)
     Serial.println(currentNotification);
     showNotification = true;
 }
+void onMediaInputChange(const std::string &message)
+{
+    currentMediaInput = new char[message.length() + 1];
+    strcpy(currentMediaInput, message.c_str());
+}
+void onSceneChange(const std::string &message)
+{
+    currentScene = new char[message.length() + 1];
+    strcpy(currentScene, message.c_str());
+}
+void onPartyButton(const std::string &message)
+{
 
+    if (message == "OFF")
+    {
+        partyButtonActivated = false;
+    }
+    else
+    {
+        partyButtonActivated = true;
+    }
+}
 
 void setupUI()
 {
@@ -101,7 +146,7 @@ void setupUI()
     ui.setIndicatorPosition(BOTTOM);
     // Defines where the first frame is located in the bar.
     ui.setIndicatorDirection(LEFT_RIGHT);
-
+    ui.disableAutoTransition();
     // You can change the transition that is used
     // SLIDE_LEFT, SLIDE_RIGHT, SLIDE_UP, SLIDE_DOWN
     ui.setFrameAnimation(SLIDE_LEFT);
@@ -114,15 +159,21 @@ void setupUI()
 
 void setup()
 {
-
+    Serial.begin(115200);
+    pinMode(D3, INPUT_PULLUP);
+    attachInterrupt(D3, handleKey, RISING);
     App.set_name("livingroom-notification");
     App.init_log();
 
-    App.init_wifi("Walhalla", "W0danHeerst");
-    auto mqttClient = App.init_mqtt("192.168.178.186", "", "");
+    App.init_wifi("Merel8bis_Boven", "eenhoorn");
+    mqttClient = App.init_mqtt("192.168.178.22", "", "");
     // App.init_ota()->start_safe_mode();
-    mqttClient->subscribe("livingroom-notification", onNotification);
+    mqttClient->subscribe("hasd/notification", onNotification);
+    mqttClient->subscribe("hasd/scene/current", onSceneChange);
+    mqttClient->subscribe("hasd/media/input", onMediaInputChange);
+    mqttClient->subscribe("stat/partybutton/POWER", onPartyButton);
     auto aswitch = App.make_gpio_binary_sensor("Switch1", 16);
+    auto rotary = App.make_rotary_encoder_sensor("RotarySwitch", D1, D2);
     App.setup();
     // Initialising the UI will init the display too.
 
@@ -130,11 +181,15 @@ void setup()
 
     display.flipScreenVertically();
 }
-void resetNotificationIcon() {
-    if (showNotification && shownNotificationCounter > 500) {
+void resetNotificationIcon()
+{
+    if (showNotification && shownNotificationCounter > 500)
+    {
         shownNotificationCounter = 0;
         showNotification = false;
-    } else {
+    }
+    else
+    {
         shownNotificationCounter++;
     }
 }
@@ -145,7 +200,23 @@ void loop()
     int remainingTimeBudget = ui.update();
     App.loop();
     resetNotificationIcon();
-    if (remainingTimeBudget > 0) {
+    // long newPosition = myEnc.read();
+    // if (newPosition != oldPosition)
+    // {
+    //     if (newPosition > oldPosition)
+    //     {
+    //         ui.nextFrame();
+    //     }
+    //     else
+    //     {
+    //         ui.previousFrame();
+    //     }
+    //     // ui.transitionToFrame(newPosition);
+    //     oldPosition = newPosition;
+    //     Serial.println(newPosition);
+    // }
+    if (remainingTimeBudget > 0)
+    {
         // You can do some work here
         // Don't do stuff if you are below your
         // time budget.
